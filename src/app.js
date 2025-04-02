48bdd47384c1d073a8b8b89045a8fe6ae75d3462
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2025-04-02 23:05:09 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,10 +13,10 @@ app.use(cookieParser());
 
 
 const corsOptions = {
-  origin: process.env.ALLOWED_SITE.split(','),
+  origin: process.env.ALLOWED_SITE,  // Make sure this matches exactly
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 };
 
 app.use(cors(corsOptions));
